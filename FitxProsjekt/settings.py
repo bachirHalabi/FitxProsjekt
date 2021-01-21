508e2f707cbbcd2c60c9aed7a4e1244a7f00c85a
bachirhalabi <Bachir97nor@gmail.com>
2021-01-21 17:42:57 +0100

    Hver gang du skal pulle bytt database settings meed ditt eget

--- FitxProsjekt/settings.py
+++ FitxProsjekt/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'FitxProsjekt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'sys',
+         'USER': 'root',
+         'PASSWORD': 'Root12345',
+         'HOST': 'localhost',
+         'PORT': '3306',
     }
 }
 
