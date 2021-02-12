459aaa6937f580946cb3974d5833720eca976cd8
bachirhalabi <Bachir97nor@gmail.com>
2021-02-12 21:41:39 +0100

    login attempt

--- FitxProsjekt/urls.py
+++ FitxProsjekt/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+from accounts import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Calories.url')),
